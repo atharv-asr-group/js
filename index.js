c225fd7d83db78f1e61fb6f2ea6cebee4cb1022c
unknown <atharv21240@iiitd.ac.in>
2023-12-31 12:14:31 +0530

    promises

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-setTimeout(function(){
-    console.log('1st')
-    setTimeout(function(){
-        console.log('3rd')
-    },2000);
-},1000);
-setTimeout(function(){
-    console.log('2nd')
-},2000);
+function medicine1get(){
+    return new Promise(function(resolve){
+        setTimeout(resolve,1000);
+    })
+}
+medicine1get().then(function(){
+    console.log('medicine 1 recieved');
+})
+// if there is a function that is asyncronous then it should
+// always return promise. JS folks said this thing.
+
+// after medicine1get is done, whatever function i call in the .then()
+// is called.
+
+
+// equivalent to: in callback way.
+
+// function medicine1get(){
+//     console.log('medicine 1 recieved');
+// }
+// setTimeout(medicine1get,1000);
