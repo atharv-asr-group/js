901f57abbf24400c1fd83432ed5944ff80d81cc3
unknown <atharv21240@iiitd.ac.in>
2024-01-01 10:24:03 +0530

    fs file read

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-function medicine1get(){
-    return new Promise(function(resolve){
-        setTimeout(resolve,1000);
-    })
+const fs = require('fs');
+function callbackFn(err,data){
+    console.log(data);
 }
-medicine1get().then(function(){
-    console.log('medicine 1 recieved');
-})
-// if there is a function that is asyncronous then it should
-// always return promise. JS folks said this thing.
-
-// after medicine1get is done, whatever function i call in the .then()
-// is called.
-
-
-// equivalent to: in callback way.
-
-// function medicine1get(){
-//     console.log('medicine 1 recieved');
-// }
-// setTimeout(medicine1get,1000);
+fs.readFile('a.txt','utf-8',callbackFn)
