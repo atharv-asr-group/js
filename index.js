ea06315e36a0de0d05a0f92e509ace50d6ccdbfd
unknown <atharv21240@iiitd.ac.in>
2024-01-01 11:05:20 +0530

    express basic

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express= require('express');
 const app= express();
 const port=3000;
+
+function calculateSum(counter){
+    var sum=0;
+    for(var i=0;i<counter;i++){
+        sum=sum+i;
+    }
+    return sum;
+}
+
 function handleFirstRequest(req,res){
-    res.send('hello world')
+    var calculatedSum=calculateSum(100);
+    console.log(calculatedSum);
+    const answer='the answer is '+calculatedSum
+    res.send(answer);
 }
 // if we get a request at /, send the control to handlefirstRequest function.
 app.get('/',handleFirstRequest)
