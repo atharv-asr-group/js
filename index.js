b32832790f939786a9e149b41c4bab29029e42d8
unknown <atharv21240@iiitd.ac.in>
2024-01-01 10:58:55 +0530

    express basic

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const fs = require('fs');
-function callbackFn(err,data){
-    console.log(data);
+const express= require('express');
+const app= express();
+const port=3000;
+function handleFirstRequest(req,res){
+    res.send('hello world')
 }
-fs.readFile('a.txt','utf-8',callbackFn)
+// if we get a request at /, send the control to handlefirstRequest function.
+app.get('/',handleFirstRequest)
+function started(){
+    console.log('example hosted on port 3000')
+}
+app.listen(port,started);
+// this code is sufficient to sort of create a website.
+
+
