c9deb5a15be5df155a0e6c6896cd165bc8f157d1
unknown <atharv21240@iiitd.ac.in>
2024-01-02 08:28:24 +0530

    arrow functions in callbacks

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-const express= require('express');
-const bodyParser=require('body-parser');
-const app= express();
-const port=3000;
-app.use(bodyParser.json());
-
-
-
-
-function calculateSum(counter){
-    var sum=0;
-    for(var i=0;i<counter;i++){
-        sum=sum+i;
-    }
-    return sum;
-}
-
-function handleFirstRequest(req,res){
-    
-    const counter=req.query.counter;
-    var calculatedSum=calculateSum(counter);
-    var answerObject={
-        sum: calculatedSum,
-        counter
-    }
-    res.send(answerObject);
-}
-
-// if we get a request at /, send the control to handlefirstRequest function.
-app.get('/',handleFirstRequest)
-function started(){
-    console.log('example hosted on port 3000')
-}
-app.listen(port,started);
-// this code is sufficient to sort of create a website.
-
-
+const fs= require('fs');
+fs.readFile('a.txt','utf-8',(err,data)=>{
+    console.log(data);
+})
