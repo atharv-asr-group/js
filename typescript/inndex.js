ac58146738d72810efd4860ed307e7454b24e73a
unknown <atharv21240@iiitd.ac.in>
2024-01-05 20:52:17 +0530

    interface example

--- typescript/inndex.js
+++ typescript/inndex.js
@@ -1,15 +1,11 @@
-interface Person{
-    name:string,
-    age:number
-}
-function greet(person:Person){
-    return "hi "+person.name+" you are "+person.age+" years old.";
+function greet(person) {
+    return "hi " + person.name + " you are " + person.age + " years old.";
 }
 // function greet(person: {
 //     name:string,
 //     age:number;
 // }):string{return "hi "+person.name+" how are you congrats on being "+person.age+" years old"}
 console.log(greet({
-    name:"atharv",
-    age:20
+    name: "atharv",
+    age: 20
 }));
